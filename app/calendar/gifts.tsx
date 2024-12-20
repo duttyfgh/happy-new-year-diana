b0428d2ad11167e1f3f193aba771df7a4ba73dd3
unknown <dutyfgh>
2024-12-20 01:13:47 +0100

    add: Little changes and correct mistakes

--- app/calendar/gifts.tsx
+++ app/calendar/gifts.tsx
@@ -5,9 +5,20 @@ import { IGift } from "@/gifts"
 import { useEffect, useState } from "react"
 import hasDatePassed from "@/scripts/has-date-passed"
 import { redirect } from "next/navigation"
+import { motion, Variants } from "framer-motion"
+
+const appearing: Variants = {
+    hidden: {
+        opacity: 0
+    },
+    visible: {
+        opacity: 1,
+    }
+}
 
 const Gift = ({ id, dateOfOpening, giftLockedImage, giftOpenedImage }: IGift) => {
     const [isOpened, setIsOpened] = useState<boolean>(false)
+    const [isTimeShowed, setIsTimeShowed] = useState<boolean>(false)
 
     useEffect(() => {
         const isPassed = hasDatePassed(dateOfOpening)
@@ -16,13 +27,31 @@ const Gift = ({ id, dateOfOpening, giftLockedImage, giftOpenedImage }: IGift) =>
 
     const onOpen = () => {
 
-        if(isOpened) {
+        if (isOpened) {
             redirect(`/calendar/${id}`)
         }
+
+        if (!isOpened) {
+            setIsTimeShowed(true)
+            setTimeout(() => {
+                setIsTimeShowed(false)
+            }, 1000)
+        }
     }
-    
+
     return (
-        <div key={id} onClick={onOpen}>
+        <div key={id} onClick={onOpen} className="relative">
+            {
+                isTimeShowed && (
+                    <motion.div
+                        className="text-[2rem] left-0 right-0 mx-auto w-fit py-2 bg-[#eddba2] top-32 absolute text-[#BF2B32] rounded-2xl px-8 shadow-lg"
+                        initial='hidden'
+                        animate='visible'
+                        variants={appearing}>
+                        <span className="font-bold">{dateOfOpening}</span>
+                    </motion.div>
+                )
+            }
             <Image
                 src={isOpened ? giftOpenedImage : giftLockedImage}
                 width={350}
